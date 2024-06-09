8cca24e2d3c8720e580913c62111a68bea63a112
K1Melo <kauafernandes.smelo@gmail.com>
2024-06-08 22:40:38 -0300

    feat: about page

--- nexus-front/src/components/AboutNexus.vue
+++ nexus-front/src/components/AboutNexus.vue
@@ -1,9 +1,31 @@
 <template>
-    <section class="about"></section>
+    <section class="about">
+        <div class="container">
+            <div class="informations">
+                <p class="item">01. <span style="color: #8526FE;" class="item">Sobre</span></p>
+                <h3 class="title">Conectando Mentes,<br>Construindo Futuros</h3>
+                <p class="text">Assim é o Nexus. Um ecossistema learning community onde mentes se encontram e colaboram
+                    para
+                    moldar o
+                    amanhã. Aqui, a troca de conhecimento é o alicerce para a construção de um futuro mais plural e
+                    democratico.</p>
+                <div class="wrapper">
+                    <button class="btn">
+                        <p class="btnText">
+                            Baixar APK</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../assets/imgs/moockup_about.png" alt="tela de cadastro" class="image">
+        </div>
+    </section>
 </template>
 
 <style scoped>
 .about {
     height: 100vh;
+    display: flex
 }
 </style>
